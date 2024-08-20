cf44c2d344de1571fa5bb478c0b356386c4590b2
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-08-20 13:17:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const state={
+    taskList:[],
+};
+
+// DOM Operations
+
+const taskContents= document.querySelector(".task__contents");
+const taskModal= document.querySelector(".task__modal__body");
+
+console.log(taskContents);
+console.log(taskModal);
+
